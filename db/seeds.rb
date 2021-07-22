436719e9af32f23cef4a5dbec47ca34246e614ca
pedro <pedropmancillac@gmail.com>
2021-07-22 16:00:24 -0400

    faltan estilos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Hacker.say_something_smart
+  content = Faker::GreekPhilosophers.quote
+  Article.create(title: title, content: content)
+end
